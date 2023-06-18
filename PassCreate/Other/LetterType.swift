3f8df96d826b941be1dd3c963d510d5333b57e53
yu3san3 <orange12817@yahoo.co.jp>
2023-06-18 21:52:19 +0900

    UserDefaultsまわりのリファクタリング

---
+++ PassCreate/Other/LetterType.swift
@@ -0,0 +1,18 @@
+//
+//  LetterType.swift
+//  PassCreate
+//
+//  Created by 丹羽雄一朗 on 2023/06/18.
+//  Copyright © 2023 Niwa Yuichirou. All rights reserved.
+//
+
+import Foundation
+
+enum LetterType: String {
+    case letterType = "letterType"
+
+    case upperCase = "upperCase"
+    case lowerCase = "lowerCase"
+    case number = "number"
+    case symbol = "symbol"
+}
